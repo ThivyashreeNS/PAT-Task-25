cba3f90c07830f6c6f238900dc48fdc490546215
ThivyashreeNS <nstshree2000@gmail.com>
2024-12-10 18:30:16 +0530

    Add files via upload

---
+++ test_imdb_search.py
@@ -0,0 +1,10 @@
+"""test_imdb_search.py"""
+import pytest
+from Task_25 import ImdbSearch
+
+def test_imdb_search():
+    obj = ImdbSearch()
+    expected_url = "https://www.imdb.com/search/name/?name=Jackie%20Chan&birth_monthday=04-07&gender=male"
+    result_url = obj.imdb_search()
+    assert expected_url == result_url
+    print("Success: Imdb Search")
